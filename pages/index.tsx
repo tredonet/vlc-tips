bdb5e32ff64a9e85b0d084e4763da07238ee94e6
Tonino Redonet <toninoredonet@gmail.com>
2022-08-01 21:29:14 +0200

    Added google maps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next';
+import { useMemo } from 'react';
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-const Home: NextPage = () =>
-  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>;
+const Home: NextPage = () => {
+  if (!process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY)
+    return <></>;
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
 
+  if (!isLoaded) return <div>Loading...</div>;
+  return <Map />;
+}
+
+function Map() {
+  const center = useMemo(() => ({ lat: 39.468, lng: -0.359 }), []);
+  return (
+    <GoogleMap zoom={14.9} center={center} mapContainerClassName="w-full h-screen">
+      <Marker position={center} />
+    </GoogleMap>
+  );
+}
 export default Home;
