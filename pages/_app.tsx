38bee108173fdeec78af8e95334e427db6a92304
Tonino Redonet <toninoredonet@gmail.com>
2023-07-31 22:47:46 +0200

    Added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
 import "../styles/globals.css";
 import { TipProvider, AuthProvider } from "providers";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <TipProvider>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
-    </TipProvider>
+    <>
+      <TipProvider>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </TipProvider>
+      <Analytics />
+    </>
   );
 }
 
