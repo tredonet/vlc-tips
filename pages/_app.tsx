f785d52df6756c05e7e944f9796e5d53a93daa31
Tonino Redonet <toninoredonet@gmail.com>
2023-09-10 13:07:14 +0200

    clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,17 @@ import "../styles/globals.css";
 import { TipProvider, AuthProvider } from "providers";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <TipProvider>
         <AuthProvider>
+          <Head>
+            <link rel="icon" href="/favicon_dark.ico" />
+            <link rel="icon" href="/favicon.ico" media="(prefers-color-scheme: dark)" />
+          </Head>
           <Component {...pageProps} />
         </AuthProvider>
       </TipProvider>
